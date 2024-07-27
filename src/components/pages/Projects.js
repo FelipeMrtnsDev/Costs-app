5b3482f283047763713e1267e06d5bfaf305d7f9
felipe martins <juninpernadecano@gmail.com>
2024-07-27 13:49:13 -0400

    versao sem projetos

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,33 @@
+import { useLocation } from 'react-router-dom'
+
+import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+
+import styles from './Projects.module.css'
+
+function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
+    
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projetos"/>
+            </div>
+            {message && <Message type='succes' msg={message} />}
+            <Container customClass='start'>
+                <p>projetos...</p>
+            </Container>
+        </div>
+    )
+}
+
+export default Projects;
